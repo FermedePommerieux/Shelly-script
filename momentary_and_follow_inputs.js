cedaff554e267ab3ae9915d8433f5824ac6341f9
FermedePommerieux <82572754+FermedePommerieux@users.noreply.github.com>
2022-11-10 15:15:26 +0100

    Update momentary_and_follow_inputs.js

--- momentary_and_follow_inputs.js
+++ momentary_and_follow_inputs.js
@@ -13,20 +13,19 @@ Shelly.call("Switch.SetConfig", {
   },
 });
 
-
-Shelly.addEventHandler(function (event) {
-  if (typeof event.info.event === "undefined") return;
+Shelly.addEventHandler(function (message) {
+  if (typeof message.info.event === "undefined") return;
   
   //act as momentary as input:0 by followin the state if switch:0
-  if (event.info.component === "switch:0") { // check if it concerns our switch
-    if (typeof event.info.state !== "undefined") { // check if it concerns state event
-      Shelly.call("Switch.Set", {'id': 1,'on':event.info.state}); // follow the state
+  if (message.info.component === "switch:0") { // check if it concerns our switch
+    if (typeof message.info.state !== "undefined") { // check if it concerns state event
+      Shelly.call("Switch.Set", {'id': 1,'on': message.info.state}); // follow the state
     }
   }
-  //act as follow as input:1
-  if (event.info.component === "input:1") { // check if it concerns our input
-    if (typeof event.info.state !== "undefined") { // check if it concerns state event
-      Shelly.call("Switch.Set", {'id': 0,'on':event.info.state}); // follow the state id:1 will be set later
+  //act as flip as input:1
+  if (meaage.info.component === "input:1") { // check if it concerns our input
+    if (typeof message.info.state !== "undefined") { // check if it concerns state event
+      Shelly.call("Switch.Set", {'id': 0,'on':message.info.state}); // follow the state id:1 will be set later
     }
   }
 }
