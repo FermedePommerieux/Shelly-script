dfa28b7c5f7a2969b03f052f5da31928453b70d0
FermedePommerieux <82572754+FermedePommerieux@users.noreply.github.com>
2022-11-10 15:22:10 +0100

    Create ShellyPlus1Thermostat.js

---
+++ ShellyPlus1Thermostat.js
@@ -0,0 +1,30 @@
+// This script makes ShellyPlus1 act as a MQTT thermostat
+// 
+
+// detachs the input : we don't need it
+Shelly.call("Switch.SetConfig", {
+  id: 1,
+  config: {
+    in_mode: "detached",
+  },
+});
+
+
+Shelly.addEventHandler(function (message) {
+  if (typeof message.info.event === "undefined") return;
+  
+  //act as momentary as input:0 by followin the state if switch:0
+  if (message.info.component === "switch:0") { // check if it concerns our switch
+    if (typeof message.info.state !== "undefined") { // check if it concerns state event
+      Shelly.call("Switch.Set", {'id': 1,'on': message.info.state}); // follow the state
+    }
+  }
+  //act as flip as input:1
+  if (meaage.info.component === "input:1") { // check if it concerns our input
+    if (typeof message.info.state !== "undefined") { // check if it concerns state event
+      Shelly.call("Switch.Set", {'id': 0,'on':message.info.state}); // follow the state id:1 will be set later
+    }
+  }
+}
+);
+
