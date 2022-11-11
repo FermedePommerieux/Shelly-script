5086f7ebf41efbc5bbf24fc021fafb82f1bb8dd8
FermedePommerieux <82572754+FermedePommerieux@users.noreply.github.com>
2022-11-11 18:15:25 +0100

    Update momentary_and_follow_inputs.js

--- momentary_and_follow_inputs.js
+++ momentary_and_follow_inputs.js
@@ -14,7 +14,7 @@ Shelly.call("Switch.SetConfig", {
 });
 
 Shelly.addStatusHandler(function (message) { 
-  //act as momentary as input:0 by followin the state if switch:0
+  //act as momentary as input:0 by following the output if switch:0
   
   if (typeof message.component === "undefined") return;
   if (message.component === "switch:0") { // check if it concerns our switch
@@ -22,7 +22,7 @@ Shelly.addStatusHandler(function (message) {
       Shelly.call("Switch.Set", {'id': 1,'on': message.delta.output}); // follow the state
     }
   }
-  //act as flip as input:1
+  //act as flip by following the state of input:1
   if (message.component === "input:1") { // check if it concerns our input
     if (typeof message.delta.state !== "undefined") { // check if it concerns state event
       Shelly.call("Switch.Set", {'id': 0,'on':message.delta.state}); // follow the state id:1 will be set later
