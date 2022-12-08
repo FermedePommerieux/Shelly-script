3774061f1d312d0d0f07a2df4fb12e33c1966953
FermedePommerieux <82572754+FermedePommerieux@users.noreply.github.com>
2022-12-08 18:18:15 +0100

    fix startup conditions

--- ShellyPlus1Thermostat.js
+++ ShellyPlus1Thermostat.js
@@ -246,7 +246,8 @@
  // create the thermostat function to load it in a timer
  function thermostat() {
  	//exit if no active MQTT connection or already running
- 	if ((!MQTT.isConnected()) && (isRunning)) {
+ 	if (!MQTT.isConnected()) return;
+    if (isRunning) {
  		Timer.clear(loadOnBootTimer_handle);
  		print('Thermostat Running, LoadOnBootTimer cleared');
  		return;
