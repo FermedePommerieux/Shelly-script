7644dd3df858bbf68fc4e3b69af1c60743485a73
FermedePommerieux <82572754+FermedePommerieux@users.noreply.github.com>
2022-12-15 08:07:41 +0100

    Update ShellyPro3-MQTT-fix.js
    fix conditions

--- ShellyPro3-MQTT-fix.js
+++ ShellyPro3-MQTT-fix.js
@@ -1,22 +1,25 @@
  // fix unexpected behavior of switch:2 if MQTT connection is lost
-let switchState = null,
-   timer = 1000,
-   timer_handle = null,
-   timerHold = null;
-   
+ let switchState = null,
+ 	wasDisconnected = null,
+ 	timer = 1000,
+ 	timer_handle = null,
+ 	timerHold = null;
+
  Shelly.addStatusHandler(function(e) {
-  if (e.component === "mqtt") {
-   if ((e.delta.connected === true)) {
-   print("here comes the bug");
-     timerHold = true;
-     switchState = Shelly.getComponentStatus('switch:2').output
-     timer_handle = Timer.set(timer, false, function () {timerHold = false;});
-     }
-   }
-  if (e.component === "switch:2") {
-     if ((timerHold) && (e.delta.source = "MQTT")) {
-       Shelly.call("Switch.set", {'id': 2, 'on': switchState});
-       Timer.clear(timer_handle);
-     }
-  }
+ 	if (e.component === "mqtt") {
+ 		if (e.delta.connected === false) wasDisconnected = true;
+ 		if ((e.delta.connected === true) && (wasDisconnected)) {
+ 			print("here comes the bug");
+ 			timerHold = true;
+ 			wasDisconnected = false;
+ 			switchState = Shelly.getComponentStatus('switch:2').output
+ 			timer_handle = Timer.set(timer, false, function() { timerHold = false; });
+ 		}
+ 	}
+ 	if (e.component === "switch:2") {
+ 		if ((timerHold) && (e.delta.source = "MQTT")) {
+ 			Shelly.call("Switch.set", { 'id': 2, 'on': switchState });
+ 			Timer.clear(timer_handle);
+ 		}
+ 	}
 });
